035bbcc820e66e1c380e2cad9518c7174067f21b
tezzachris <65645957+tezzachris@users.noreply.github.com>
2023-12-11 21:11:52 +0100

    Add files via upload

---
+++ logliknn.m
@@ -0,0 +1,44 @@
+function [LL,lls,Xt] = logliknn (theta, yt, zt)
+
+n = size(yt,1);
+
+alpha0 = theta(1);
+alpha1 = theta(2);
+beta1 = theta(3);
+beta2 = theta(4);
+vary = theta(5);
+varx = theta(6);
+
+%initials
+Xt0 = mean(yt);
+zt0 = zt(1);
+
+Pt = eye(1,1);
+Xt = zeros(n,1);
+Z = alpha1;
+lls = zeros(n,1);
+
+    for i = 1 : n 
+        if i > 1
+            Xt0 = Xt(i-1); zt0 = zt(i);
+        end
+        Xpred =  beta1*Xt0 + beta2*zt0; 
+
+        %Prediction equations
+        ypred = alpha0 +  Z * Xpred ;
+        Ppred = beta1^2 * Pt + varx ;
+
+        %Prediction error
+        vt = yt(i)  - ypred ;
+        Ft = Z * Ppred * Z' + vary ;
+    
+        %Updating equations
+        Kt = (Ppred * Z')/(Ft);
+        Xt(i) = Xpred + (Kt * vt')';  
+        Pt =  Ppred - Kt*Z*Ppred;
+        lls(i) = - 0.5*( log(2*pi) + log( det(Ft) ) +  (vt / Ft * vt') ); %vt needs to be column vector
+    end
+    LL = sum(lls);
+    lls = -lls;
+    LL = -LL;
+end
